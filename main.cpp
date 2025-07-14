41ffd312429d545b52729507a8859d0a6fa205d6
rmrc14 <remin1459@gmail.com>
2025-07-14 12:38:56 +0530

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include "gw_basic/GWBasic64.h"
+#include <iostream>
+
+int main(int argc, char* argv[]) 
+{
+    GWBasic64 basic;
+    try 
+    {
+        if (argc > 1) 
+        {
+            basic.loadAndRunFile(argv[1]);  // File mode .bas file is given directly
+        }
+        else 
+        {
+            basic.runREPL();                // REPL mode READ EVAL PRINT LOOP
+        }
+    }
+    catch (const std::exception& e) 
+    {
+        std::cerr << "Fatal Error: " << e.what() << std::endl;
+    }
+    return 0;
+}
