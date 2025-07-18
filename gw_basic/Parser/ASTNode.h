809e7d5ef32304b12b13559be399e8ff4027e8d2
suve0803 <suve0803@gmail.com>
2025-07-18 10:39:32 +0530

    updated

--- gw_basic/Parser/ASTNode.h
+++ gw_basic/Parser/ASTNode.h
@@ -1,5 +1,4 @@
 #pragma once
-#include <memory>
 #include <string>
 #include <vector>
 
@@ -14,53 +13,60 @@ enum class ASTType {
 
 class ASTNode {
 public:
-    virtual ~ASTNode() = default;
+    virtual ~ASTNode() {}
     virtual ASTType type() const = 0;
 };
 
-using AST = std::shared_ptr<ASTNode>;
+using ASTList = std::vector<ASTNode*>;
 
 class ProgramNode : public ASTNode {
 public:
-    std::vector<AST> stmts;
+    ASTList stmts;
+    ~ProgramNode() {
+        for (auto s : stmts) delete s;
+    }
     ASTType type() const override { return ASTType::Program; }
 };
 
 class PrintNode : public ASTNode {
 public:
-    AST expr;
-    explicit PrintNode(AST e) : expr(std::move(e)) {}
+    ASTNode* expr;
+    PrintNode(ASTNode* e) : expr(e) {}
+    ~PrintNode() { delete expr; }
     ASTType type() const override { return ASTType::PrintStmt; }
 };
 
 class LetNode : public ASTNode {
 public:
     std::string name;
-    AST expr;
-    LetNode(std::string n, AST e)
-        : name(std::move(n)), expr(std::move(e)) {}
+    ASTNode* expr;
+    LetNode(const std::string& n, ASTNode* e)
+        : name(n), expr(e) {}
+    ~LetNode() { delete expr; }
     ASTType type() const override { return ASTType::LetStmt; }
 };
 
 class NumberNode : public ASTNode {
 public:
     std::string value;
-    explicit NumberNode(std::string v) : value(std::move(v)) {}
+    NumberNode(const std::string& v) : value(v) {}
     ASTType type() const override { return ASTType::NumberExpr; }
 };
 
 class IdentNode : public ASTNode {
 public:
     std::string name;
-    explicit IdentNode(std::string n) : name(std::move(n)) {}
+    IdentNode(const std::string& n) : name(n) {}
     ASTType type() const override { return ASTType::IdentExpr; }
 };
 
 class BinOpNode : public ASTNode {
 public:
     std::string op;
-    AST left, right;
-    BinOpNode(std::string o, AST l, AST r)
-        : op(std::move(o)), left(std::move(l)), right(std::move(r)) {}
+    ASTNode* left;
+    ASTNode* right;
+    BinOpNode(const std::string& o, ASTNode* l, ASTNode* r)
+        : op(o), left(l), right(r) {}
+    ~BinOpNode() { delete left; delete right; }
     ASTType type() const override { return ASTType::BinOpExpr; }
 };
