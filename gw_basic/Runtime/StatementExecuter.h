6e88393bffe825a0875f641b7c5b7da823586630
NivyaPrakash <nivyaprakash2000@gmail.com>
2025-07-18 23:49:13 +0530

    added

--- gw_basic/Runtime/StatementExecuter.h
+++ gw_basic/Runtime/StatementExecuter.h
@@ -1,8 +1,30 @@
-#pragma once
+/*#pragma once
 
 class StatementExecuter {
 public:
     StatementExecuter();
     //void StatementExecuter::execute(const ASTNode&);
     // todo by nivya 
+};
+*/
+
+
+#pragma once
+#include "ASTNode.h"
+#include "SymbolTable.h"
+#include "ExpressionEvaluator.h"
+
+class StatementExecutor {
+public:
+    StatementExecutor(SymbolTable& table);
+
+    void execute(ASTNode* node);
+
+private:
+    SymbolTable& table_;
+    ExpressionEvaluator evaluator_;
+
+    void executePrint(PrintNode* printNode);
+    void executeLet(LetNode* letNode);
+    Value evaluateExpr(ASTNode* exprNode);
 };
---
+++ gw_basic/Runtime/SymbolTable.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <unordered_map>
+#include <string>
+#include "TypeSystem.h"
+
+class SymbolTable {
+public:
+    void setVariable(const std::string& name, const Value& value);
+    Value getVariable(const std::string& name) const;
+
+private:
+    std::unordered_map<std::string, Value> variables_;
+};
